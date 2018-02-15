ae12de76409fb6b7aa9febd3a7f2592930bbd4c9
eduard <cf17eduard.corral@iesjoandaustria.org>
2018-02-15 12:53:48 +0100

    añadido p6

--- PRACTICA 6/practica6.py
+++ PRACTICA 6/practica6.py
@@ -15,9 +15,26 @@ def AfegirNota(alumnos,nota1,nota2,nota3):
 	print"Escriu la nota de la tercera avaluació"
 	nota=int(raw_input())
 	nota3.append(nota)
+	
+def mostrarNota(alumnos,nota1,nota2,nota3):
+	print "Alumnos  1ra     2na     3ra     final"
+	for i in range (len(alumnos)):
+		print alumnos[i]," ",nota1[i]," ",nota2[i]," ",nota3[i]," ",(nota1[i]+nota2[i]+nota3[i]
+
+
 
 
 alumnos=[]
 notas1=[]
 notas2=[]
 notas3=[]
+
+opcio=(raw_input())
+if opcio=="a"
+	AfegirNota(alumnos,notas1,notas2,notas3)
+if opcio=="m":
+	mostrarNota(alumnos,notas1,notas2,notas3)
+	
+	
+	
+	
