d1e8d786bd11656151cba96c1e6f59cdaf54b4ab
eduard <cf17eduard.corral@iesjoandaustria.org>
2018-03-21 19:55:20 +0100

    projecto2

---
+++ PROJECTE2/projecto2.java
@@ -0,0 +1,85 @@
+/*
+ * projecto2.java
+ * 
+ */
+
+
+public class sense títol {
+	public static BufferedReader abrirArchivo(String ruta){
+     File archivo = null;
+     BufferedReader br = null;
+     try {
+         // Apertura del fichero y creacion de BufferedReader para poder
+         // hacer una lectura comoda (disponer del metodo readLine()).
+         archivo = new File (ruta);
+         br = new BufferedReader(new FileReader (archivo));
+     }catch (IOException ex) {
+            System.out.println(ex);
+            }
+       return br;
+      }
+    
+    
+    public static void leerArchivo(BufferedReader br)throws IOException{
+		String linea;
+         while((linea=br.readLine())!=null){
+			System.out.println(linea);
+			}
+		}
+	
+   public static void cerrarArchivo(BufferedReader br){
+	   try{                    
+            if(null!=br  ){   
+               br.close();     
+                }                  
+         }catch (Exception e2){ 
+            e2.printStackTrace();
+         }
+	   }
+	
+	
+	public static void escribirArchivo(String ruta,int cuantasLineas,BufferedReader reader)throws IOException{
+		FileWriter fichero = null;
+        PrintWriter pw = null;
+		try{
+		fichero = new FileWriter(ruta);
+		pw = new PrintWriter(fichero);
+		for (int i = 0; i < cuantasLineas; i++){
+			System.out.println("Escribe texto de linea "+i);
+			String texto;
+			texto=reader.readLine();
+			pw.println( texto +" Numero de linea: "+i);
+		}
+		} catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+           try {
+           // Nuevamente aprovechamos el finally para 
+           // asegurarnos que se cierra el fichero.
+           if (null != fichero)
+              fichero.close();
+           } catch (Exception e2) {
+              e2.printStackTrace();
+           }
+		}
+	}
+	
+	public static void main (String args[]) {
+		
+	}
+}
+
+
+
+
+
+
+
+
+SET AUTOCOMMIT=1;
+
+CREATE DATABASE IF NOT EXISTS sanitat charset latin1;
+
+USE sanitat;
+
+CREATE TABLE IF NOT EXISTS sanitat.HOSPITAL (
